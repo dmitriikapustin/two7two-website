3b87f1fab001521cbc480fb25eaf22dcc6d1f3d9
dmitriikapustin <dmitriikapustin@ya.ru>
2022-10-14 21:15:09 +0300

    DOM in process

--- src/scripts/heading-animation.js
+++ src/scripts/heading-animation.js
@@ -2,18 +2,17 @@ $(document).ready(function () {
 
     // Set ScrollMagic controller 
     var controller_heading_anim = new ScrollMagic.Controller({container: "body"})
-    var heading_anim_duration = 2000;
 
 
     // ScrollMagic animation for circles timeline
-    var scene = new ScrollMagic.Scene({ triggerElement: '#big-text', duration: heading_anim_duration, reverse: true })
-        .offset($(window).height() * 1 + 400)
+    var scene = new ScrollMagic.Scene({ triggerElement: '#big-text', duration: 1000, reverse: true })
+        .offset($('#big-text').height() / 2)
         .setTween(".animated_heading_item", { scale: 0.25 } )
         // .addIndicators()
         .addTo(controller_heading_anim);
 
     var scene = new ScrollMagic.Scene({ triggerElement: '#big-text', duration: 1000, reverse: true })
-        .offset($(window).height() * 1 + 650)
+        .offset($('#big-text').height() / 2)
         .setPin( '#big-text' )
         // .addIndicators()
         .addTo(controller_heading_anim); 
