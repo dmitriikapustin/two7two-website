99f66bc8298c05668ad7b60f167d21019ca1df36
RettPrattr <desaswork@gmail.com>
2022-10-16 13:47:03 +0300

    before pulling main 16.10

--- src/scripts/custom-cursor.js
+++ src/scripts/custom-cursor.js
@@ -2,8 +2,37 @@ $(document).ready(function () {
 
   // #TASK Create new one 
 
-  var $c = $("[data-custom-cursor]");
-  var $h = $(".words-wrapped");
+  var $c = $(".cursor-container");
+  var body = $('body')
+  // var $h = $(".animated_heading_item");
+
+  var lastScrollTop = 0;
+
+  // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+  window.addEventListener("scroll", function () { // or window.addEventListener("scroll"....
+    var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+    if (st > lastScrollTop) {
+      // downscroll code
+      $(window).scroll(function () {
+        $('.cursor.active')
+          .eq(0)
+          // .css({
+          //   transform: 'translate3d(0, -100%, 0)'
+          // });
+      });
+    } else {
+      // upscroll code
+      $(window).scroll(function () {
+        $('.cursor.active')
+          .eq(0)
+          // .css({
+          //   transform: 'translate3d(0, 100%, 0)'
+          // });
+      });
+    }
+    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+  }, false);
+
   // var $i = $("img");
 
   $(window).on("mousemove", function (e) {
@@ -14,30 +43,23 @@ $(document).ready(function () {
     $c.css("left", x);
   });
 
-  $h.on("mouseenter", function (e) {
-    $c.addClass("custom-cursor-active");
-  });
+  // $h.on("mouseenter", function (e) {
+  //   $c.addClass("custom-cursor-active");
+  // });
 
-  $h.on("mouseleave", function (e) {
-    $c.removeClass("custom-cursor-active");
-  });
+  // $h.on("mouseleave", function (e) {
+  //   $c.removeClass("custom-cursor-active");
+  // });
 
-  $(document).mousemove(function (e) {
-    $('.cursor')
-      .eq(0)
-      .css({
-        left: e.pageX,
-        top: e.pageY,
-        opacity: 1
-      });
-  })
+  // $(document).mousemove(function (e) {
+  //   $('.cursor.active')
+  //     .eq(0)
+  //     .css({
+  //       left: e.pageX,
+  //       top: e.pageY,
+  //       opacity: 1
+  //     });
+  // })
 
-  $(window).scroll(function () {
-    $('.cursor')
-      .eq(0)
-      .css({
-        opacity: 0
-      });
-  });
 
 });
