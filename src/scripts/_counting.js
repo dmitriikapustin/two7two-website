c3a7f993efca5187f8f73a0a46da9755fbea92ac
RettPrattr <desaswork@gmail.com>
2022-10-31 20:54:52 +0300

    commit for commit

--- src/scripts/_counting.js
+++ src/scripts/_counting.js
@@ -1,62 +1,31 @@
 $(document).ready(function () {
-    if ($(window).width() > 800) {
-        let counterBoolean = false;
-        var counters = $(".count");
-        var countersQuantity = counters.length;
-        var counter = [];
-      
-        for (i = 0; i < countersQuantity; i++) {
-          counter[i] = parseInt(counters[i].innerHTML);
-        }
-      
-        var count = function(start, value, id) {
-          var localStart = start;
-          setInterval(function() {
-            if (localStart < value) {
-              localStart++;
-              counters[id].innerHTML = localStart + " +";
-            }
-          }, 40);
-        }
-      
-    
-        $(window).on('scroll', function () {
-            if ($(window).scrollTop() >= $('.block-4').offset().top + $('.block-4').outerHeight() - window.innerHeight && counterBoolean === false)  {
-                counterBoolean = true;
-                for (j = 0; j < countersQuantity; j++) {
-                    count(0, counter[j], j);
-                }
-            }
-        });
-    
-    } else {
-        let counterBoolean = false;
-        var counters = $(".count");
-        var countersQuantity = counters.length;
-        var counter = [];
-      
-        for (i = 0; i < countersQuantity; i++) {
-          counter[i] = parseInt(counters[i].innerHTML);
-        }
-      
-        var count = function(start, value, id) {
-          var localStart = start;
-          setInterval(function() {
-            if (localStart < value) {
-              localStart++;
-              counters[id].innerHTML = localStart + " +";
-            }
-          }, 40);
-        }
-      
-    
-        $(window).on('scroll', function () {
-            if ($(window).scrollTop() >= $('.block-4').offset().top + $('.block-4').outerHeight() - window.innerHeight && counterBoolean === false)  {
-                counterBoolean = true;
-                for (j = 0; j < countersQuantity; j++) {
-                    count(0, counter[j], j);
-                }
-            }
-        });
+    let counterBoolean = false;
+    var counters = $(".count");
+    var countersQuantity = counters.length;
+    var counter = [];
+  
+    for (i = 0; i < countersQuantity; i++) {
+      counter[i] = parseInt(counters[i].innerHTML);
     }
+  
+    var count = function(start, value, id) {
+      var localStart = start;
+      setInterval(function() {
+        if (localStart < value) {
+          localStart++;
+          counters[id].innerHTML = localStart + " +";
+        }
+      }, 40);
+    }
+  
+
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() >= $('.block-4').offset().top + $('.block-4').outerHeight() - window.innerHeight && counterBoolean === false)  {
+            counterBoolean = true;
+            for (j = 0; j < countersQuantity; j++) {
+                count(0, counter[j], j);
+            }
+        }
+    });
+
 })
